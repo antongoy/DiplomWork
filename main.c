75781745da3807aaf3c9710aeb7168386063ad03
antongoy <g.a.s.s@mail.ru>
2015-03-18 18:35:05 +0300

    Add fill_row_cn function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <string.h>
 
 #define $(i, j) ((i) * 3 + (j))
-
+#define $$(i, j) ((i) * 3 + (j) + 9)
 
 inline lapack_complex_double * create_matrix(void) {
     return (lapack_complex_double *)calloc(3 * 3, sizeof(lapack_complex_double));
@@ -79,10 +79,52 @@ void fill_row_cn(lapack_complex_double *row,
                  lapack_complex_double *K,
                  lapack_complex_double *M,
                  int i, int j, int k, int l, int r, int s) {
+
     int row_size = ((3 * 3) * 2);
     memset(row, 0, sizeof(lapack_complex_double) * row_size);
 
+    int rcx, scx, icx, jcx, kcx, lcx;
 
+    rcx = map_index_x(r);
+    scx = map_index_x(s);
+    icx = map_index_x(i);
+    jcx = map_index_x(j);
+    kcx = map_index_x(k);
+    lcx = map_index_x(l);
+
+    row[$(r, s)] = A[$(i, j)] * B[$(k, l)];
+    row[$(rcx, scx)] = A[$(icx, jcx)] * B[$(kcx, lcx)];
+    
+    int rny, sny, iny, jny, kny, lny;
+
+    rny = map_index_y(r);
+    sny = map_index_y(s);
+    iny = map_index_y(i);
+    jny = map_index_y(j);
+    kny = map_index_y(k);
+    lny = map_index_y(l);
+    
+    row[$$(r, s)] = K[$(i, j)] * M[$(k, l)];
+    row[$$(rny, sny)] = K[$(iny, jny)] * M[$(kny, lny)];
+
+    int rnz, snz, inz, jnz, knz, lnz;
+
+    rnz = map_index_z2(r);
+    snz = map_index_z2(s);
+    inz = map_index_z2(i);
+    jnz = map_index_z2(j);
+    knz = map_index_z2(k);
+    lnz = map_index_z2(l);
+
+    r = map_index_z1(r);
+    s = map_index_z1(s);
+    i = map_index_z1(i);
+    j = map_index_z1(j);
+    k = map_index_z1(k);
+    l = map_index_z1(l);
+
+    row[$$(r, s)] = K[$(i, j)] * M[$(k, l)];
+    row[$$(rnz, snz)] = K[$(inz, jnz)] * M[$(knz, lnz)];
 }
 
 
