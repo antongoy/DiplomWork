d25ee68129ee2fc4db2f096b35c50d103f5d0f2f
antongoy <g.a.s.s@mail.ru>
2015-03-18 18:17:59 +0300

    Add functions for the mapping indexes. Start fill_rows functions.

--- main.c
+++ main.c
@@ -37,6 +37,55 @@ inline void print_matrix(lapack_complex_double *matrix) {
 }
 
 
+inline int map_index_x(int i) {
+    switch (i) {
+        case 0: return 0;
+        case 1: return 2;
+        case 2: return 1;
+    }
+    return i;
+}
+
+
+inline int map_index_y(int i) {
+    switch (i) {
+        case 0: return 2;
+        case 1: return 1;
+        case 2: return 0;
+    }
+    return i;
+}
+
+
+inline int map_index_z1(int i) {
+    return map_index_x(i);
+}
+
+
+inline int map_index_z2(int i) {
+    switch (i) {
+        case 0: return 2;
+        case 1: return 0;
+        case 2: return 1;
+    }
+    return i;
+}
+
+
+
+void fill_row_cn(lapack_complex_double *row,
+                 lapack_complex_double *A,
+                 lapack_complex_double *B,
+                 lapack_complex_double *K,
+                 lapack_complex_double *M,
+                 int i, int j, int k, int l, int r, int s) {
+    int row_size = ((3 * 3) * 2);
+    memset(row, 0, sizeof(lapack_complex_double) * row_size);
+
+
+}
+
+
 int main(void) {
     lapack_complex_double *A;
 
